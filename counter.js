d97d4e0b46815de8c6377f0bc5170918c57cab36
Ronny Roethof <ronny@roethof.net>
2017-01-24 14:28:00 +0100

    update

--- counter.js
+++ counter.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+var fs = require('fs');
+
+var search = function(dir, needle) {
+	  if(!fs.existsSync(dir)) {
+		      return console.log('Directory ' + dir + ' does not exist.');
+		    }
+	  
+	var haystack = fs.readdirSync(dir), path, stats;
+	  for(var s = 0; s < haystack.length; s++) {
+		      path = dir + '/' + haystack[s];
+		      stats = fs.statSync(path);
+		      
+		  if(stats.isDirectory()) {
+			        search(path, needle);
+			      } else if(path.indexOf(needle) >= 0) {
+			        console.log(path);
+			      }
+		    }
+};
+
+search(process.argv[2], process.argv[3]);
