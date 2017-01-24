c58c8380d842f3b477084a8714ba5eb8ede03027
Ronny Roethof <ronny@roethof.net>
2017-01-24 12:01:06 +0100

    Changed from webserver to stdout message

--- counter.js
+++ counter.js
@@ -1,14 +1,1 @@
-var http = require('http');
-
-var userCount = 0;
-http.createServer(function (request, response) {
-	    console.log('New connection');
-	    userCount++;
-
-	    response.writeHead(200, {'Content-Type': 'text/plain'});
-	    response.write('Hello!\n');
-	    response.write('We have had '+userCount+' visits!\n');
-	    response.end();
-}).listen(80);
-
-console.log('Server started');
+console.log('Hello World');
